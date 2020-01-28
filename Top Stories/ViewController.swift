608f63f5d63e21dc000f1fec3093ff8b209c4b48
Trey Cysewski <treycysewski@yahoo.com>
2020-01-28 10:42:56 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Trey Cysewski on 1/28/20.
+//  Copyright © 2020 Trey Cysewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
